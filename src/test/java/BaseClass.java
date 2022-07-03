22d13c16b7ff7090746087e035fa4e774fa4476a
MJ77777 <106645719+MJ77777@users.noreply.github.com>
2022-07-03 12:55:31 -0400

    changing website

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -2,7 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
