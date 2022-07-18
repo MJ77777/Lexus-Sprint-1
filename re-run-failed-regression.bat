012344f767705d407f01780b9fffcd84f89093ed
MJ77777 <106645719+MJ77777@users.noreply.github.com>
2022-07-18 13:14:40 -0400

    adding new xml files

---
+++ re-run-failed-regression.bat
@@ -0,0 +1,2 @@
+call mvn clean test -D xmlFile=Regression.xml
+call mvn test -D xmlFile=target/surefire-reports/testng-failed.xml
--- re-run-failed.bat
+++
@@ -1,2 +0,0 @@
-call mvn clean test -DxmlFile=smoke-test.xml
-call mvn test -DxmlFile=target/surefire-reports/testng-failed.xml
